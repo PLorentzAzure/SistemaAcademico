1aa67727336feea790f702680127833ab1ee2cef
PLorentz <plorentz@AvellPA.com>
2016-06-27 23:10:05 -0300

    Criando backdoor para validação de token.

--- SistemaAcademico/SistemaAcademico.Servico/Controllers/BackDoorController.cs
+++ SistemaAcademico/SistemaAcademico.Servico/Controllers/BackDoorController.cs
@@ -9,17 +9,28 @@ using System.Web.Http.Description;
 using SistemaAcademico.Dominio;
 using SistemaAcademico.Servico.Controllers.Base;
 using SistemaAcademico.Negocio.Gerenciador.Auxiliar;
+using SistemaAcademico.Servico.Seguranca;
 
 namespace SistemaAcademico.Servico.Controllers
 {
     public class BackDoorController : Controlador
     {
         [HttpGet]
+        [Route("api/BackDoor/"+nameof(ResetarBase))]
         public IHttpActionResult ResetarBase()
         {
             new GerenciadorBackDoor().ResetarBancoDeDados();
 
             return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/BackDoor/"+nameof(DadosToken))]
+        public IHttpActionResult DadosToken([FromBody]string token)
+        {
+            InformacaoToken info;
+            EmissorTokenFactory.BuscaEmissorToken().ValidaToken(token, out info);
+            return Ok(info);
         }
     }
 }
